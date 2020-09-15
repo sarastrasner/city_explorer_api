013abdb661a82700a8532abc659770a19f299979
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2020-09-15 13:44:39 -0700

    second feature complete

--- server.js
+++ server.js
@@ -1,49 +1,54 @@
 'use strict';
 
-// this will bring in my express libraray
-const express = require('express');
-const cors = require('cors');
-
-// bring in the dotenv library
-// the job of this library is to find the .env file and get the variables out of it so we can use them in our JS file
+//get environmental variables from .env
 require('dotenv').config();
 
-// this gives us a variable that we can use to run all the methods that are in the express library
-const app = express();
+//pull in dependencies
+const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
 
+
+//Application setup
+const PORT = process.env.PORT;
+const app = express();
 app.use(cors());
 
-
-// the dotenv library lets us grab the PORT var from the .env using the magic words process.env.variableName
-const PORT = process.env.PORT;
-
-app.get('/', function (request, response) {
-  response.send('Hello World');
-});
-
+//Routes
 app.get('/location', handleLocation);
 app.get('/weather', handleWeather);
 app.use('*', notFoundHandler);
 
 
 function handleLocation(request, response) {
-  try {
-    const geoData = require('./data/location.json');
-    const city = request.query.city;
-    const locationData = new Location(city, geoData);
-    response.send(locationData);
+  let city = request.query.city;
+  let key = process.env.GEOCODE_API_KEY;
+  const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+
+  if (request[url]) {
+    response.send(request[url]);
   }
-  catch (error) {
-    console.log('ERROR', error);
-    response.status(500).send('Sorry, something went wrong.');
+  else {
+    superagent.get(url)
+      .then(data => {
+        console.log(data);
+        const geoData = data.body[0];
+        const location = new Location(city, geoData);
+        request[url] = location;
+        response.send(location);
+      })
+      .catch((error) => {
+        console.log('ERROR', error);
+        response.status(500).send('So sorry, something went wrong.');
+      });
   }
 }
 
 function Location(city, geoData) {
   this.search_query = city;
-  this.formatted_query = geoData[0].display_name;
-  this.latitude = geoData[0].lat;
-  this.longitude = geoData[0].lon;
+  this.formatted_query = geoData.display_name;
+  this.latitude = geoData.lat;
+  this.longitude = geoData.lon;
 }
 
 
@@ -59,7 +64,7 @@ function handleWeather(request,response) {
     response.send(weatherData);
   }
   catch (error) {
-    console.log('ERROR', error);
+    console.log('ERROR');
     response.status(500).send('Sorry, something went wrong.');
   }
 }
