c94f4c988cb75add970a28c7532eeb853028dbff
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2020-09-17 18:52:31 -0700

    lab 9 complete

--- schema.sql
+++ schema.sql
@@ -1,9 +1,26 @@
+DROP TABLE IF EXISTS weather;
 
 DROP TABLE IF EXISTS citydata;
 
+
 CREATE TABLE citydata (
   id SERIAL PRIMARY KEY,
-  city_name VARCHAR(255),
-  latitude INT,
-  longitude INT
-)
+  search_query VARCHAR(255),
+  formatted_search_query VARCHAR(255),
+  latitude decimal,
+  longitude decimal
+);
+
+-- SELECT * FROM citydata;
+
+
+CREATE TABLE weather (
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  forecast VARCHAR(255),
+  forecast_time VARCHAR(255),
+  search_timestamp VARCHAR(255)
+);
+
+-- SELECT * FROM weather;
+
